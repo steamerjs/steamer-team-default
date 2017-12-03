30dfe7f9f4dcde0fd8ab4c8fa6cbab1b99cfc350
lcxfs1991 <lcxfs1991@gmail.com>
2017-12-03 23:09:48 +0800

    update package.json keywords

--- index.js
+++ index.js
@@ -5,40 +5,33 @@ let teamConfig = {
     },
     kits: [
         {
-            'steamer-react': {
-                'git': 'https://github.com/steamerjs/steamer-react.git'
-            }
+            'name': 'steamer-react',
+            'git': 'https://github.com/steamerjs/steamer-react.git'
         },
         {
-            'steamer-vue': {
-                'git': 'https://github.com/steamerjs/steamer-vue.git'
-            }
+            'name': 'steamer-vue',
+            'git': 'https://github.com/steamerjs/steamer-vue.git'
         },
         {
-            'steamer-simple': {
-                'git': 'https://github.com/steamerjs/steamer-simple.git'
-            }
+            'name': 'steamer-simple',
+            'git': 'https://github.com/steamerjs/steamer-simple.git'
         },
         {
-            'steamer-react-component': {
-                'git': 'https://github.com/steamerjs/steamer-react-component.git'
-            }
+            'name': 'steamer-react-component',
+            'git': 'https://github.com/steamerjs/steamer-react-component.git'
         },
         {
-            'steamer-vue-component': {
-                'git': 'https://github.com/steamerjs/steamer-vue-component.git'
-            }
+            'name': 'steamer-vue-component',
+            'git': 'https://github.com/steamerjs/steamer-vue-component.git'
         },
         {
-            'steamer-simple-component': {
-                'git': 'https://github.com/steamerjs/steamer-simple-component.git'
-            }
+            'name': 'steamer-simple-component',
+            'git': 'https://github.com/steamerjs/steamer-simple-component.git'
         },
         {
-            'steamer-logic-component': {
-                'git': 'https://github.com/steamerjs/steamer-logic-component.git'
-            }
-        },
+            'name': 'steamer-logic-component',
+            'git': 'https://github.com/steamerjs/steamer-logic-component.git'
+        }
     ],
     plugins: [
         'steamer-plugin-mock'
